3d6e8c811bf841d5e09b02da1d1ae779c6556eef
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-25 11:58:10 +0330

    create table view

--- ToDoListApp/ViewController.swift
+++ ToDoListApp/ViewController.swift
@@ -8,13 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - properties
+    
+    let cellIdentifier: String = "myCell"
+    
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.backgroundColor = .white
     }
 
 
 }
 
+
+// MARK: - table view delegate
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - table view data source
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.backgroundColor = .white
+        return cell
+        
+    }
+    
+}
