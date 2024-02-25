3e02f9ca911d1e9787b262f3ee88a4fd723e883e
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-25 12:21:03 +0330

    story board design

---
+++ ToDoListApp/ContentToDoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContentToDoViewController.swift
+//  ToDoListApp
+//
+//  Created by Omid Heydarzadeh on 2/25/24.
+//
+
+import UIKit
+
+class ContentToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ ToDoListApp/NewToDoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoViewController.swift
+//  ToDoListApp
+//
+//  Created by Omid Heydarzadeh on 2/25/24.
+//
+
+import UIKit
+
+class NewToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
