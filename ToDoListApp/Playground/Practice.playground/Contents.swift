fcf2702e7210977d550397ba0cbf0058108c8eb7
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-26 07:18:58 +0330

    code four

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -4,6 +4,15 @@ var greeting = "Hello, playground"
 
 // UserDefaults.standard.set ========= UserDefaults().set
 
+
+// Storing a Value
+UserDefaults.standard.set(true, forKey: "condition")
+
+// Retrieving a Value
+let boolFromUD = UserDefaults.standard.bool(forKey: "condition")
+print(boolFromUD)
+
+
 UserDefaults.standard.set(true, forKey: "number")
 print(UserDefaults.standard.bool(forKey: "number"))
 
@@ -11,5 +20,10 @@ UserDefaults.standard.set([12, 13, 14], forKey: "array")
 print(UserDefaults.standard.array(forKey: "array") ?? "")
 
 
+UserDefaults().set(23, forKey: "twentyThree")
+
+let age = UserDefaults().integer(forKey: "twentyThree")
+
+print(age)
 
 
