5fd9043d962f1d0a6a63bcba9b43215b138e0c74
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-28 00:16:28 +0330

    code nine

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -2,20 +2,32 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-// UserDefaults.standard.set ========= UserDefaults().set
+let myURL = URL(string: "https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg")
 
+func fetch() {
+    
+    guard let url = myURL else { return }
+    
+    let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
+        
+        if let error = error { print("there is an fetching error: \(error.localizedDescription)") }
+        
+        guard let data = data else { return }
+        
+        UserDefaults.standard.set(data, forKey: "imageData")
+        
+        print(data)
+        
+    }
+    
+    
+    dataTask.resume()
+}
 
-// Storing a Value
-UserDefaults.standard.set(true, forKey: "condition")
+fetch()
 
-// Retrieving a Value
-let boolFromUD = UserDefaults.standard.bool(forKey: "condition")
-print(boolFromUD)
+let myImage = UserDefaults.standard.data(forKey: "imageData")
 
+let uiImage = UIImage(data: myImage!)
 
-
-UserDefaults().set(12, forKey: "omid")
-
-if (UserDefaults().value(forKey: "omid") != nil) {
-    print("ok")
-} else { print("no") }
+print(uiImage!)
