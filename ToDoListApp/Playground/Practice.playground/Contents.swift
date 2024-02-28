2ef8a800404df26be0be1162059dcf81cf73eded
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-28 18:24:49 +0330

    code eleven

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -26,3 +26,30 @@ let viewController = MyViewController()
 
 // Present the view controller in the live view
 PlaygroundPage.current.liveView = viewController
+
+// MARK: - New View
+
+class myViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let button2 = UIButton(type: .system)
+        
+        button2.setTitle("button2", for: .normal)
+        button2.frame = CGRect(x: 120, y: 120, width: 200, height: 200)
+        button2.addTarget(self, action: #selector(button2Tapped), for: .touchUpInside)
+        
+        view.addSubview(button2)
+        
+    }
+    
+    @objc func button2Tapped() {
+        print("Button 2 is Tapped!")
+    }
+    
+}
+
+let viewController2 = myViewController2()
+
+PlaygroundPage.current.liveView = viewController2
