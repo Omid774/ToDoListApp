753f75a647d58f8b10d61cfd41f118e546bb0501
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-28 14:55:36 +0330

    code ten

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -1,33 +1,28 @@
 import UIKit
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
-
-let myURL = URL(string: "https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg")
-
-func fetch() {
-    
-    guard let url = myURL else { return }
-    
-    let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
+// Create a view controller
+class MyViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        if let error = error { print("there is an fetching error: \(error.localizedDescription)") }
+        // Create a button
+        let button = UIButton(type: .system)
+        button.setTitle("Tap Me", for: .normal)
+        button.frame = CGRect(x: 200, y: 200, width: 100, height: 50)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         
-        guard let data = data else { return }
-        
-        UserDefaults.standard.set(data, forKey: "imageData")
-        
-        print(data)
-        
+        // Add the button to the view
+        view.addSubview(button)
     }
     
-    
-    dataTask.resume()
+    @objc func buttonTapped() {
+        print("Button tapped!")
+    }
 }
 
-fetch()
+// Instantiate the view controller
+let viewController = MyViewController()
 
-let myImage = UserDefaults.standard.data(forKey: "imageData")
-
-let uiImage = UIImage(data: myImage!)
-
-print(uiImage!)
+// Present the view controller in the live view
+PlaygroundPage.current.liveView = viewController
