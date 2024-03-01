3882d2f1453c7ff8d94c8ea7f1300607d0d0bdc4
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-01 11:54:33 +0330

    code twenty

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,4 +5,12 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
+func type(_ input: Any) {
+    
+    print(type(of: input))
+    
+}
 
+let json = URL(string: "https://reactnative.dev/movies.json")!
+
+type(json)
