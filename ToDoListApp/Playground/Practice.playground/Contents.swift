63162243d4dda824bc45a5d61a4691e26beef046
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-01 12:02:08 +0330

    code twenty one

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,7 +5,15 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-func type(_ input: Any) {
+protocol T {
+    
+}
+
+extension URL: T {
+    
+}
+
+func type(_ input: T) {
     
     print(type(of: input))
     
