e063192df9034b4acb90237fca55640914ed57e1
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-01 10:27:43 +0330

    code eighteen

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -1,40 +1,8 @@
 import UIKit
 
-let url = "https://reactnative.dev/movies.json"
+// https://jsonplaceholder.typicode.com/posts
+// https://reactnative.dev/movies.json
+// https://api.github.com/repos/:owner/:repo
+// https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-func fetch() {
-    
-    guard let url = URL(string: url) else { return }
-    
-    let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
-        
-        if let error = error { print("there is an fetching error: \(error.localizedDescription)") }
-        
-        guard let data = data else { return }
-        
-        print(data)
-        print("data: \(type(of: data))")
-        
-        let count = data.count
-        
-        print(count)
-        print("count: \(type(of: count))")
-        
-    }
-    
-    dataTask.resume()
-    
-}
 
-fetch()
-
-class Person: Hashable {
-    
-    var name: String
-    var lastName: String
-    
-    init(_ name: String, _ lastName: String) {
-        self.name = name; self.lastName = lastName
-    }
-    
-}
