e6cb84d993ca52739335a48de8f69c87256a0cda
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-02 23:02:47 +0330

    code twenty five

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,56 +5,13 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-/*
- 
- @objc func saveTask() {
-     
-     guard let text = field.text, !text.isEmpty else { return }
-     
-     guard let count = UserDefaults.standard.value(forKey: "count") as? Int else { return }
-     
-     let newCount = count + 1
-     
-     UserDefaults.standard.set(newCount, forKey: "count")
-     
-     UserDefaults.standard.set(text, forKey: "task_\(newCount)")
-     
-     update?() // if this method exist call else nothing or nil
-     
-     navigationController?.popViewController(animated: true)
-     
- }
- 
- */
-
-
-// textfield, text
-
-var textField = UITextField()
-let navigationController = UINavigationController()
-
-
-func update() {}
-
-UserDefaults.standard.set(0, forKey: "count")
-
-
-func saveTask() {
+func findSQRT(number: Int) -> Int {
     
-    guard let text = textField.text, !text.isEmpty else { return }
+    var start = 0
+    var end = number
+    var mid = -1
+    var ans = -1
     
-    guard let count = UserDefaults.standard.value(forKey: "count") as? Int else { return }
     
-    let newCount = count + 1
-    
-    UserDefaults.standard.set(newCount, forKey: "count")
-    
-    UserDefaults.standard.set(text, forKey: "task_\(newCount)")
-    
-    update()
-    
-    navigationController.popViewController(animated: true)
     
 }
-
-saveTask()
