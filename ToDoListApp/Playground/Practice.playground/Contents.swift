1ccf2d46b50a45377e94d0c42e9e63cf9f729615
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-03 00:18:21 +0330

    code twenty eight

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,36 +5,7 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-func findSQRT(number: Int) -> Int {
-    
-    var start = 0
-    var end = number
-    var mid = -1
-    var ans = -1
-    
-    while (start <= end) {
-        
-        mid = (start + end) / 2
-        if (mid * mid == number) {
-            ans = mid
-            break
-        }
-        if mid * mid < number {
-            ans = start
-            start = mid + 1
-        } else {
-            end = mid - 1
-        }
-        
-    }
-    return ans
-    
+while (true) {
+    print("ok")
+    break
 }
-
-print(16, " sqrt => ", findSQRT(number: 16))
-print(25, " sqrt => ", findSQRT(number: 25))
-
-let twentyFive = findSQRT(number: 25)
-UserDefaults.standard.set(twentyFive, forKey: "omid")
-UserDefaults.standard.value(forKey: "omid")
-
