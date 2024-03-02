0d965fba2e9ae8dd921494374bc9eb62cbd3c61d
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-02 23:06:30 +0330

    code twenty six

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -12,6 +12,24 @@ func findSQRT(number: Int) -> Int {
     var mid = -1
     var ans = -1
     
-    
+    while (start <= end) {
+        
+        mid = (start + end) / 2
+        if (mid * mid == number) {
+            ans = mid
+            break
+        }
+        if mid * mid < number {
+            ans = start
+            start = mid + 1
+        } else {
+            end = mid - 1
+        }
+        
+    }
+    return ans
     
 }
+
+print(16, " sqrt => ", findSQRT(number: 16))
+print(25, " sqrt => ", findSQRT(number: 25))
