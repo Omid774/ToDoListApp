fb1ba7e911bf022126707adf4dd010077a6a58f8
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-04 00:58:29 +0330

    code thirty two

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,28 +5,10 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-while (true) {
-    print("ok")
-    break
-}
-
-extension Float {
+enum omid: Optional<Any> {
+    typealias RawValue = Error
     
-    static func ~=(valueA: Float, valueB: Float) -> Bool {
-        return abs(valueA - valueB) <= 0.01
-    }
+    
+    case onee
     
 }
-
-var floatA: Float = 10.01
-var floatB: Float = 10.01
-
-floatB ~= floatA
-
-var myAge: Optional<Int> = .some(23)
-var myAge2: Optional<Int> = .none
-
-Bool(0)
-
-Bool(234)
-Bool(-234)
