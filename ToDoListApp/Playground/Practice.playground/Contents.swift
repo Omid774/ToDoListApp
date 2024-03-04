f0c292ef0b6b7100e57c664e0ace8f715d148658
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-04 19:56:27 +0330

    code thirty five

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,7 +5,22 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-var set = ([1, 2])
 
-var sett: Set<Any> = [1, "ok", "omid", Float(3.5), 4.5]
-print(sett)
+class A: Hashable {
+    static func == (lhs: A, rhs: A) -> Bool {
+        
+        if lhs == rhs { return true }
+        
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        print("ok")
+    }
+    
+    
+    let name: String = "AAA"
+    
+}
+
+
+// inout
