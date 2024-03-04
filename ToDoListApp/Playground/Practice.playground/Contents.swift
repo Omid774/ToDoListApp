ae292371e63aab657c84beed38f788f40dd53025
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-04 19:53:24 +0330

    code thirty four

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,6 +5,7 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-enum omid: String {
-    case one, two, three
-}
+var set = ([1, 2])
+
+var sett: Set<Any> = [1, "ok", "omid", Float(3.5), 4.5]
+print(sett)
