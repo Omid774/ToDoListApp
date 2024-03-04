c7483946cc4279699962b0f4db61202a048b2b36
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-04 22:27:14 +0330

    code thirty seven

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -6,21 +6,8 @@ import UIKit
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
 
-let myURL = "https://reactnative.dev/movies.json"
-
-func fetch() {
+while (true) {
+    print("ok")
     
-    guard let url = URL(string: myURL) else { return }
-    
-    let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
-        
-        if let error = error { print("there is an error: \(error.localizedDescription)") }
-        
-        guard let data = data else { return }
-        
-        print(data)
-        
-    }
-    
-    dataTask.resume()
+    break
 }
