e62d2a6d3ed48190a99b057f0b59969a89600a54
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-05 19:32:16 +0330

    code fourty one

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -6,19 +6,15 @@ import UIKit
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
 
-let a = Float(2.5)
-let b: Float = 6.4
-
-if a ~= b {
-    
-}
-
-extension Float {
-    
-    static func ~=(valueA: Float, valueB: Float) -> Bool {
-        if abs(valueA - valueB <= 0.01) {
-            return true
-        }
+class A: Equatable {
+    static func == (lhs: A, rhs: A) -> Bool {
+        <#code#>
     }
     
+    var name: Int = 0
 }
+
+let a = A()
+let b = A()
+
+print(a === b)
