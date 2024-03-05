683fa8229d1b800b6a170be1417ec58787b9e7a8
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-05 19:40:29 +0330

    code fourty two

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -6,15 +6,36 @@ import UIKit
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
 
-class A: Equatable {
-    static func == (lhs: A, rhs: A) -> Bool {
-        <#code#>
-    }
+let myURL = URL(string: "https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg")
+
+func fetch() {
     
-    var name: Int = 0
+    guard let url = myURL else { return }
+    
+    let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
+        
+        if let error = error { print("there is an fetching error: \(error.localizedDescription)") }
+        
+        guard let data = data else { return }
+        
+        print("FLAG 1: ", data)
+        
+        convertToImage(data: data)
+        
+    }
+  
+    dataTask.resume()
+    
 }
 
-let a = A()
-let b = A()
+func convertToImage(data: Data?) {
+    
+    guard let data = data else { return }
+    
+    guard let image = UIImage(data: data) else { return }
+    
+    print("FLAG 2: ", image)
+    
+}
 
-print(a === b)
+fetch()
