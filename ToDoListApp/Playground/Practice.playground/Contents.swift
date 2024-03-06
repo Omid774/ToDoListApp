902f0b5340e693265b6062dea4c4868ddef0ffb0
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-06 18:59:12 +0330

    defer

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,60 +5,16 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
+var a = 10
 
-let myURL = URL(string: "https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg")
-
-func fetch() {
+if a == 10 {
     
-    guard let url = myURL else { return }
-    
-    let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
-        
-        if let error = error { print("there is an fetching error: \(error.localizedDescription)") }
-        
-        guard let data = data else { return }
-        
-        print("FLAG 1: ", data)
-        
-        convertToImage(data: data)
-        
-    }
-  
-    dataTask.resume()
-    
-}
-
-func convertToImage(data: Data?) {
-    
-    guard let data = data else { return }
-    
-    guard let image = UIImage(data: data) else { return }
-    
-    print("FLAG 2: ", image)
-    
-}
-
-fetch()
-
-
-class A: Equatable {
-    static func == (lhs: A, rhs: A) -> Bool {
-        <#code#>
+    defer {
+        a += 5
     }
     
+    print("a is ten")
     
 }
 
-let a = A()
-let b = A()
-
-print(a !== b)
-
-
-func plus(_ x: Int, _ y: Int) -> Int {
-    
-    return x + y
-    
-}
-
-plus(2, 3)
+print(a)
