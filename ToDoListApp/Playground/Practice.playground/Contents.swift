bf42aa5fbb2dc32b8112bbd06880c9c20b9707ce
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-07 00:16:40 +0330

    factorial

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,24 +5,14 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-var a = 10
-
-if a == 10 {
-    
-    defer {
-        a += 5
+func factorial(_ n: Int) -> Int {
+    if n == 0 {
+        return 1
     }
-    
-    print("a is ten")
-    
-    defer {
-        print("second defer")
-    }
-    
-    defer {
-        print("third defer")
-    }
-    
+    return n * factorial(n - 1)
 }
 
-print(a)
+// Example usage:
+let number = 5
+let result = factorial(number)
+print("Factorial of \(number) is \(result)")
