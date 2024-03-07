e9bcf072fd43d54a70c45cfd8c484c6bdd531651
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-07 23:01:10 +0330

    db

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,17 +5,27 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-class Person {
+class DbConnection {
     
-}
-
-class Car {
+    var connection: String
+    var dbConnection: String?
+    var finishHandler: (() -> Void)? = nil
     
-    var model: String
-    var owner: Person? = nil
+    deinit {
+        finishHandler = nil
+        closeConnection()
+    }
     
-    init() {
-        self.model = "BMW"
+    init(connection: String) {
+        self.connection = connection
+    }
+    
+    func openConnection() {
+        dbConnection = "open"
+    }
+    
+    func closeConnection() {
+        dbConnection = "close"
     }
     
 }
