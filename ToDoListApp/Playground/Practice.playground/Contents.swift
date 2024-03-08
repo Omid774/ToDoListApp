eedfc805b7ff0e40f65ed213cf32eea53da4820d
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-08 16:26:17 +0330

    enum optional

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,15 +5,10 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-enum Barcode {
-    
-    case upc(Int, Int, Int, Int)
-    case qrCode(String)
-    
+enum Beverage: Int {
+    case coffee
+    case tea
 }
 
-let omid = Barcode.upc(4, 4, 4, 4)
-print(omid)
-
-let myQRCode = Barcode.qrCode("abc123")
-print(myQRCode)
+let tea = Beverage(rawValue: 10)
+print(tea)
