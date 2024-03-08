21f41c00010c90c4398f81604f6c41d0d22d68fe
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-08 16:14:50 +0330

    barcode

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,10 +5,15 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-protocol A {
+enum Barcode {
+    
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
     
 }
 
-func do(a: A) {
-    
-}
+let omid = Barcode.upc(4, 4, 4, 4)
+print(omid)
+
+let myQRCode = Barcode.qrCode("abc123")
+print(myQRCode)
