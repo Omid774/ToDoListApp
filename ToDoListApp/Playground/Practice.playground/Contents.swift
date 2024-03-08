4ade118f990d252c0cad1d2f32045a841bd68001
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-08 15:56:17 +0330

    CaseIterable in enum

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,13 +5,16 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-enum UploadItem {
-    case image(String)
-    case pdf(String)
-    case video(URL)
+enum Beverage: CaseIterable {
+    case coffee, tea, juice
 }
 
-let uploadItem1 = UploadItem.image
-let uploadItem2 = UploadItem.pdf
+Beverage.allCases
 
-uploadItem1 == uploadItem2
+print(Beverage.allCases)
+
+let omidc = Beverage.allCases
+
+for beverage in Beverage.allCases {
+    print(beverage)
+}
