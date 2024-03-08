e55e53801645868b7a35c627c0f018f512a3b865
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-08 15:47:48 +0330

    enum

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,47 +5,13 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-@propertyWrapper
-struct ValidRange {
-    
-    var value: Int
-    var min: Int
-    var max: Int
-    
-    var wrappedValue: Int {
-        
-        get {
-            value
-        }
-        
-        set {
-            
-            if newValue < min {
-                value = min
-            } else if newValue > max {
-                value = max
-            } else {
-                value = newValue
-            }
-            
-        }
-        
-    }
-    
-    init(wrappedValue: Int, min: Int, max: Int) {
-        self.value = wrappedValue
-        self.min = min
-        self.max = max
-    }
-    
+enum UploadItem {
+    case image(String)
+    case pdf(String)
+    case video(URL)
 }
 
+let uploadItem1 = UploadItem.image
+let uploadItem2 = UploadItem.pdf
 
-struct Car {
-    
-    @ValidRange(min: 5, max: 35) var fuel: Int = 0
-    
-}
-
-var car = Car(fuel: 7)
-car.fuel = 38
+uploadItem1 == uploadItem2
