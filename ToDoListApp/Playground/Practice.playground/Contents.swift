bdd8bd13fe040cac790ea912c1a41288da526c00
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-09 00:10:56 +0330

    raw enum value

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,10 +5,14 @@ import UIKit
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-enum Beverage: Int {
-    case coffee
-    case tea
+enum OmidDevices: Int {
+    case iPhone = 45
+    case Mac
+    case Monitor
+    case Speaker
 }
 
-let tea = Beverage(rawValue: 10)
-print(tea)
+let omidPhone = OmidDevices.iPhone
+print(omidPhone.rawValue)
+
+OmidDevices.Speaker.rawValue
