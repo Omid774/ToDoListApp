1987c9ccfc48487cfaace65373b6055ea7691fc1
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-09 16:14:01 +0330

    class

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,5 +5,10 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-var initialFood: [(name: String, price: Int)] = []
-initialFood = [(name: "omid", price: 34)]
+public class A {
+    
+}
+
+class B: A {
+    
+}
