5099d09562864a3b876dad3572fa8d3de3a08348
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-10 00:14:35 +0330

    private(set)

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,12 +5,9 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-class A {
+class Player {
     
-    func b() { c() }
-    private func c() {}
+    var name: String = ""
+    private(set) var play: Bool = false
     
 }
-
-var a = A()
-a.b()
