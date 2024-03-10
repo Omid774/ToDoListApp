88caadc663f859b31b4ba9178468c1ddba12d8b0
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-11 00:28:42 +0330

    class2

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,44 +5,10 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-class omid {
-    
-    var x: Float
-    var y: Float
-    
-    var z: Float {
-        get {
-            x + y
-        }
-        set(newValue) {
-            x = newValue
-            print(newValue)
-        }
-    }
-    
-    init(x: Float, y: Float) {
-        self.x = x
-        self.y = y
-    }
+class A {
     
 }
 
-let omidInstance = omid.init(x: 3, y: 1)
-omidInstance.z
-omidInstance.x
-print(omidInstance.x)
-
-
-let omidInstance2 = omid.init(x: 2, y: 1)
-omidInstance2.z
-omidInstance2.x
-
-@available (iOS 13, *)
-func a() { }
-
-
-func aa() {
-    
-    func b() {}
+class B {
     
 }
