028da8af70fdbcb221f8f7c0b6529cf978d0c743
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-10 22:37:45 +0330

    if case let

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,16 +5,12 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-class Omid {
-    
-    var name: String = ""
-    
-    deinit {
-        print("end")
-    }
-    
+
+var names = ["omid", "hasan", "kimia"]
+
+if case let omid = names[0] {
+    print("ok")
+    print(omid)
 }
 
-var omidInstance: Omid? = Omid()
 
-omidInstance = nil
