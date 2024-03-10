fde26806709487f496cc1c16c7b5cf436769a3bf
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-10 22:56:30 +0330

    closure

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -6,11 +6,10 @@ import Foundation
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
 
-var names = ["omid", "hasan", "kimia"]
+var myClosure: Optional<() -> Void> = nil
 
-if case let omid = names[0] {
-    print("ok")
-    print(omid)
+myClosure = {
+    print("Hello")
 }
 
-
+myClosure
