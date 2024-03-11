9e294d0c11ad99a1c5ebdb175bb2c5903917def7
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-11 16:48:28 +0330

    hash

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,22 +5,17 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-class A {
-   
-    static func a() { }
-    class func b() { }
+struct Person: Hashable {
     
-    
-}
-
-class B: A {
-    
-    override class func b() {
-        print("")
-    }
+    var name: String
+    var age: Int
     
 }
 
+let person1 = Person(name: "omid", age: 23)
+let person2 = Person(name: "hasan", age: 40)
 
-func a() {}
-func b() {}
+var dictionary: [Person: String] = [:]
+
+dictionary[person1] = "ok"
+dictionary[person2] = "no"
