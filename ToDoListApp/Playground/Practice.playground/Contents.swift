dac3ae0504f23ae5d4385406d1dfbdabc662d84a
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-12 00:19:34 +0330

    closure

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,32 +5,8 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-struct Person: Hashable {
+func omid(completion: @escaping () -> Void) {
     
-    var name: String
-    var age: Int
+    completion()
     
 }
-
-let person1 = Person(name: "omid", age: 23)
-let person2 = Person(name: "hasan", age: 40)
-
-var dictionary: [Person: String] = [:]
-
-dictionary[person1] = "ok"
-dictionary[person2] = "no"
-
-func fetch() {
-    print("hello", #function)
-}
-
-var fetch1: Optional<() -> Void> = {
-    print("hello", #function)
-}
-
-guard fetch1() else { return }
-fetch()
-
-fetch1 = nil
-
-fetch1()
