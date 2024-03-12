e35d3c8d8e8bdd151452e9767aa7c2412bbcf53a
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-12 19:18:01 +0330

    problem

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,13 +5,19 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-struct omid {
-    var name: String
+class omid {
+    var name: String = ""
 }
 
 extension omid {
-    init(ـ name: String) {
+    convenience init(ـ name: String) {
 //        print("ok")
-        self.name = name
+        self.init(ـ: self.name,: name)
     }
+}
+
+extension omid {
+    
+    
+    
 }
