0cdd28423ab4d2c3cb3880e4e423c0d152c28388
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-13 00:37:38 +0330

    in objective c we have not enum

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,11 +5,10 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-var a = 25
-
-repeat {
-    print(a)
-    a += 5
-} while a < 35
-
+@objc
+enum Omid: Int {
+    typealias RawValue = 8
+    
+    
+}
 
