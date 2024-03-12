59c78a9bb1c6a8945ba4cc08c3763bea942987b5
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-13 00:34:10 +0330

    repeat while

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,12 +5,11 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-var arrayOfStrings = [String]()
+var a = 25
 
-arrayOfStrings.append("omid")
-
-for case let value as String in arrayOfStrings {
-    print(value)
-}
+repeat {
+    print(a)
+    a += 5
+} while a < 35
 
 
