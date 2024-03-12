078c0845d519aa57ad6e6379fcd084c0d4a419bd
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-13 00:28:21 +0330

    order in line for compile is matter and important

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,19 +5,12 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-class omid {
-    var name: String = ""
+var arrayOfStrings = [String]()
+
+arrayOfStrings.append("omid")
+
+for case let value as String in arrayOfStrings {
+    print(value)
 }
 
-extension omid {
-    convenience init(ـ name: String) {
-//        print("ok")
-        self.init(ـ: self.name,: name)
-    }
-}
 
-extension omid {
-    
-    
-    
-}
