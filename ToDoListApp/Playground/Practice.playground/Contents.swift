1956a9b78c93f36d07829882b6c721a652d347de
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-13 22:12:05 +0330

    switch one ok dige bemire check one bag ro

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -6,36 +6,14 @@ import Foundation
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
 
-let myURL = "https://reactnative.dev/movies.json"
+let a = 10
+let b = 15
 
-func fetch() {
-    
-    guard let url = URL(string: myURL) else { return }
-    
-    let task = URLSession.shared.dataTask(with: url) { data, _, error in
-        
-        if let error = error { print("there is an fetching error: \(error.localizedDescription)") }
-        
-        guard let data = data else { return }
-        
-        print(data)
-        
-        let byteInt: Int? = data.count
-        
-        print("\(convertByteToBit(byteInt)) bit")
-        
-    }
-   
-    task.resume()
-    
+switch (a, b) {
+case (let a, 15):
+    print(a)
+case (_, let b):
+    print(b)
+default:
+    print("d")
 }
-
-fetch()
-
-func convertByteToBit(_ byte: Int?) {
-    
-    let bit: Int = (byte ?? 0) * 8
-    print(bit)
-    
-}
-
