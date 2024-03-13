ae766c68bfe0025f84bb8f7c90c349b8e62f01e1
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-14 00:22:26 +0330

    url session config

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -6,8 +6,14 @@ import Foundation
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
 
-func omid() -> (min: Int, max: Int) {
-    
-    return(2, 4)
-    
+func omid(f: () -> Void) {
+    f()
 }
+
+omid(f: ff)
+
+func ff() {}
+
+let config = URLSessionConfiguration.background(withIdentifier: "unique")
+
+let session = URLSession(configuration: config)
