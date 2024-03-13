d194e48169506884c7ab749fb844d3b50bb464b7
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-13 22:20:34 +0330

    functions can return tuple

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -6,22 +6,8 @@ import Foundation
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
 
-let a = 10
-
-if a == 10 {
+func omid() -> (min: Int, max: Int) {
     
-    print("ok")
-    
-    defer {
-        print("first defer")
-    }
-    
-    print("no")
-    
-    defer {
-        print("second defer")
-    }
+    return(2, 4)
     
 }
-
-
