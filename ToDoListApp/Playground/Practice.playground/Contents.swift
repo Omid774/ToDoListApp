89bb68ca57a611c9a16a7632f76d458cf886b30c
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-14 12:25:28 +0330

    convenience initializer

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -6,14 +6,23 @@ import Foundation
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
 
-func omid(f: () -> Void) {
-    f()
+class Bird {
+    
+    var name: String
+    var age: Int
+    var isDead: Bool
+    
+    var owner: String
+    
+    init(_ name: String, _ age: Int, _ isDead: Bool, owner: String) {
+        self.name = name
+        self.age = age
+        self.isDead = isDead
+        self.owner = owner
+    }
+    
+    convenience init(name: String, age: Int) {
+        self.init(name, age, false, owner: "omid")
+    }
+    
 }
-
-omid(f: ff)
-
-func ff() {}
-
-let config = URLSessionConfiguration.background(withIdentifier: "unique")
-
-let session = URLSession(configuration: config)
