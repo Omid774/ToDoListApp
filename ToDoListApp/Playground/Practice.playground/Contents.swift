d4a45cab4ed9e3797af2d1c2ca68a0c3fc23750c
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-15 14:50:27 +0330

    lldb

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -5,13 +5,8 @@ import Foundation
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
 
-class A {
-    static func b() {}
-    class func c() {}
+enum Beverage: CaseIterable {
+    case tea, juice, coffee
 }
 
-class B: A {
-    override class func c() {
-        print("ok")
-    }
-}
+print(Beverage.allCases)
