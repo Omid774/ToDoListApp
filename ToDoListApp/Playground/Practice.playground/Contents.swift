6b17bd1b9225ccfff715c01694b00b079bc26156
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-16 00:23:21 +0330

    associated type and type alias

--- ToDoListApp/Playground/Practice.playground/Contents.swift
+++ ToDoListApp/Playground/Practice.playground/Contents.swift
@@ -6,33 +6,14 @@ import UIKit
 // https://reactnative.dev/movies.json
 // https://api.github.com/repos/:owner/:repo
 // https://elcomercio.pe/resizer/wNOHq2Ey0866Ur5dfLfA3C5TcP4=/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/D5DQJROBSFH2DAL2TVL26I6MLE.jpg
-
-var myClosure: ( () -> Void )? // declare
-
-myClosure = { // implementation
-    print("omid")
+protocol hasan {
+    associatedtype omid = Int
 }
 
-myClosure!() // call
-
-
-protocol A {
-    
+class iman: hasan {
+    let integer: omid = 5
 }
 
-class Benz: A {
-    
-    var myClosure: ( () -> Void )? = nil
-    
-}
 
-class Bmw: A {
-    
-    func omid() {
-        var myBenzInstance = Benz() as Benz
-        myBenzInstance.myClosure = {
-            print("ok")
-        }
-    }
-    
-}
+typealias car = String
+
